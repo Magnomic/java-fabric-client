15d478d1b9386b0f1eb779f7116a3a4910eb7e98
Du <duhaiwen@gmail.com>
2020-07-13 19:47:42 +0800

    update test

--- src/test/java/org/example/ClientTest.java
+++ src/test/java/org/example/ClientTest.java
@@ -11,8 +11,8 @@ public class ClientTest {
 	@Test
 	public void testFabCar() throws Exception {
 		String org = "org2";
-		EnrollAdmin.main(org);
-		ClientApp clientApp = new ClientApp(RegisterUser.main(org));
+		EnrollAdmin.main();
+		ClientApp clientApp = new ClientApp();
 		clientApp.run(org);
 
 	}
